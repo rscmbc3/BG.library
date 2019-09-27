cd22475fa9b712c444a1a9c3a8de433133a7d77b
GS\lgorman <rscmb_c3@yahoo.com>
2019-09-27 09:00:52 -0400

    remove commented code

--- BG.library/R/makeXaxis.R
+++ BG.library/R/makeXaxis.R
@@ -1,5 +1,5 @@
 makeXaxis<-function(addSetting, settingOverlay,xDomain){
-#if (addSetting[1]=="" | settingOverlay){ 
+
  xAxis<- paste("xaxis  = list(tickmode = 'array',
                tickvals = seq(startTime,endTime,1),
                ticks = 'outside',
@@ -12,29 +12,7 @@ makeXaxis<-function(addSetting, settingOverlay,xDomain){
                tickangle = 60
                
   )")
-#}else if (addSetting[1]!="" & !settingOverlay){
- # xAxis<- paste("xaxis  = list(showtickLabels = FALSE,
-#showline = FALSE,"
-#                ,xDomain,"##
 
-#showTitle = FALSE),#
-#
-#                xaxis2 = list(tickmode = 'array',
-#               tickvals = seq(startTime,endTime,1),
-#               ticks = 'outside',
-##               ticktext = xticks,
-#               tick0 = xticks[1],",
-#                xDomain,"      
-#               #domain = c(0,0.80),
-#               zeroline = FALSE,
-#               showline = TRUE,
-#               tickangle = 60,
-#side = 'bottom'
-#               
-#  )")
-#  print(xAxis)
-  
-#}
   return(xAxis)
 
 }
